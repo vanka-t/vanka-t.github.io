7d9769cf3085382c76f93fc669070611995bb023
vanka-t <vt2068@nyu.edu>
2022-04-14 08:40:05 -0400

    set timer

---
+++ puppy-game/game.js
@@ -0,0 +1,31 @@
+
+//timer settings
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+
+            //FLASH GAME OVER AND RESET BUTTON HERE gameOver()
+
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinute = 60,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+};
+
+function gameOver(){
+
+}
