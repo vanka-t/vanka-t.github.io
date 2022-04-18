967144ac12918b2fc98c7043033c011240bc8f14
vanka-t <vt2068@nyu.edu>
2022-04-18 00:28:06 -0400

    Welcome banner + timer done

--- puppy-game/game.js
+++ puppy-game/game.js
@@ -1,34 +1,68 @@
 
 //timer settings
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+
+
+const startContainer = document.getElementById('start-container');
+const startButton = document.getElementById('.startButton');
+
+const resetContainer = document.getElementById('reset-container');
+const resetButton = document.getElementById('.resetButton');
+
+
+
+function startGame(){ // start/welcome page with the popup banner
+    resetContainer.classList.add('remove');
+   startContainer.addEventListener("click", ()=>{ // IF START BUTTON IS CLICKED:
+    startContainer.classList.add('remove'); //WELCOME POPUP BANNER IS REMOVED
+        var halfMinute = 30,//  --> change here if the seconds of the timer
+        display = document.querySelector('#time');
+        startTimer(halfMinute, display);
+        timer = duration; //TIMER STARTS
+
+        //you'll prolly want to insert your code somewhere *** HERE ***
+    
+    });
+}
+
+
+
+function startTimer(duration, display) { //TIMER SETTINGS 
+    var timer = duration, seconds;
     setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
+        //minutes = parseInt(timer / 60, 10); -->this is optional incase we want to show minutes, otherwise ignore
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
+        //minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        display.textContent = seconds;//minutes + ":" + seconds;
 
-        if (--timer < 0) {
+        if (--timer === 0) { //IF TIMER GOES TO 00:00:
             
-            gameOver();
-            timer = duration; //ideally have it freeze
-
-            //FLASH GAME OVER AND RESET BUTTON HERE gameOver()
+            gameOver();    //SHOW GAME OVER PAGE
 
         }
     }, 1000);
 }
 
-window.onload = function () {
-    var oneMinute = 5,//60,
-        display = document.querySelector('#time');
-    startTimer(oneMinute, display);
-};
-
 function gameOver(){
-    document.querySelector('#bg').classList.add('bgChange0');
+
+    resetContainer.classList.add('add');
+    resetContainer.addEventListener("click", ()=>{
+        startGame();
+        count = 0
+       // updateDisplay();
+        
+    
+    });
+    
+    document.querySelector('#bg').classList.remove('body');
+    document.querySelector('#bg').classList.add('gameOver');
 
 }
+
+window.onload = function () { // this is the main function that calls the chain of functions
+    startGame()
+    document.querySelector('#bg').classList.add('body');
+};
+
