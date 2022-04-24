105ed8a14c524c6270edc7c7577a10e54861c3a7
vanka-t <vt2068@nyu.edu>
2022-04-24 16:11:53 -0400

    character applied

--- pokemon/game.js
+++ pokemon/game.js
@@ -10,7 +10,18 @@ c.fillRect(0,0,canvas.width,canvas.height);
 const bckground = new Image()
 bckground.src = 'assets-prova/map.png'
 
-bckground.onload = () => {
-    c.drawImage(bckground,-800,-350)// change starting position of background
+const playerImg = new Image() //FRONT CHAR
+playerImg.src = 'assets-prova/char2-front.png' 
 
+bckground.onload = () => {
+    c.drawImage(bckground,-825,-335)// change starting position of background
+    playerImg.onload = () => {
+        c.drawImage(
+            playerImg,
+            canvas.width/2 - playerImg.width/2,
+             canvas.height/2 - playerImg.height/2);
+    }
+    console.log()
+   // c.fillRect(0,0,playerImg.width,playerImg.height);
 }
+
