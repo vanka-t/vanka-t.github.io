e62788af3e11ceb4b10904c595e3dc5b419051b7
vanka-t <vt2068@nyu.edu>
2022-04-24 14:35:04 -0400

    canvas applied

--- pokemon/game.js
+++ pokemon/game.js
@@ -1,1 +1,16 @@
 const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d') //ctx = context
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'blue'
+c.fillRect(0,0,canvas.width,canvas.height);
+
+const bckground = new Image()
+bckground.src = 'assets-prova/map.png'
+
+bckground.onload = () => {
+    c.drawImage(bckground,-800,-350)// change starting position of background
+
+}
