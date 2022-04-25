4f8655734df76d26aef6e3eb60a109c723574a39
vanka-t <vt2068@nyu.edu>
2022-04-25 01:26:27 -0400

    establishing key commands

--- pokemon/game.js
+++ pokemon/game.js
@@ -12,16 +12,120 @@ bckground.src = 'assets-prova/map.png'
 
 const playerImg = new Image() //FRONT CHAR
 playerImg.src = 'assets-prova/char2-front.png' 
+let lastKey = '' //empty str for memory key
 
-bckground.onload = () => {
-    c.drawImage(bckground,-825,-335)// change starting position of background
-    playerImg.onload = () => {
-        c.drawImage(
-            playerImg,
-            canvas.width/2 - playerImg.width/2,
-             canvas.height/2 - playerImg.height/2);
+
+
+const keys = {
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
     }
-    console.log()
-   // c.fillRect(0,0,playerImg.width,playerImg.height);
+}
+class Sprite {
+    constructor({pos, velocity, bckground}) {
+        this.pos = pos //position
+        this.bckground = bckground 
+    }
+
+    draw(){
+        c.drawImage(this.bckground, this.pos.x, this.pos.y)// change starting position of background
+    }
 }
 
+const background = new Sprite({pos: {
+    x: -825,
+    y: -335 
+    },
+    bckground: bckground
+})
+
+
+function animate() {
+    window.requestAnimationFrame(animate) 
+    background.draw()
+   // playerImg.onload = () => {
+        c.drawImage(
+            playerImg,
+            //cropping of char.png
+            0, //xpos
+            0, //ypos
+            playerImg.width/4,
+            playerImg.height,
+            //actual position of char
+            canvas.width/2 - (playerImg.width/4) /2, //precise location for far left sprite in cropped png
+            canvas.height/2 - playerImg.height/2,
+            playerImg.width/4,
+            playerImg.height
+        )
+    //}
+    if (keys.ArrowUp.pressed){ //bckground view goes down
+        background.pos.y += 3
+        console.log(" y: " , background.pos.y)
+    } 
+    if (keys.ArrowDown.pressed){//bckground view goes up
+        background.pos.y -= 3
+        console.log(" y: " , background.pos.y)
+    } 
+    if (keys.ArrowLeft.pressed){ //bckground view goes left
+        background.pos.x += 3
+        console.log(" x: " , background.pos.x)
+    } 
+    if (keys.ArrowRight.pressed){ //bckground view goes left
+        background.pos.x -= 3
+        console.log(" x: " , background.pos.x)
+    } 
+}
+
+window.addEventListener('keydown', (e) => { // === mousePressed
+    //console.log(e.key)
+    switch (e.key) { //if keyPressed
+        case 'ArrowUp':
+            console.log(" hihi up u go")
+            keys.ArrowUp.pressed = true
+            break
+        case 'ArrowDown':
+            console.log(" huhu down u go")
+            keys.ArrowDown.pressed = true
+            break
+        case 'ArrowLeft':
+            console.log(" hehe left u go")
+            keys.ArrowLeft.pressed = true
+            break
+        case 'ArrowRight':
+            console.log(" hoho right u go")
+            keys.ArrowRight.pressed = true
+            break
+    }
+}) 
+
+window.addEventListener('keyup', (e) => { //equivalent to mouseReleased
+    //console.log(e.key)
+    switch (e.key) { //if keyPressed
+        case 'ArrowUp':
+            console.log(" hihi up u go")
+            keys.ArrowUp.pressed = false
+            break
+        case 'ArrowDown':
+            console.log(" huhu down u go")
+            keys.ArrowDown.pressed = false
+            break
+        case 'ArrowLeft':
+            console.log(" hehe left u go")
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowRight':
+            console.log(" hoho right u go")
+            keys.ArrowRight.pressed = false
+            break
+    }
+}) 
+animate();
