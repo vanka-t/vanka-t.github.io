f73f38cf670734090b48c1150cef6dda6f636710
vanka-t <vt2068@nyu.edu>
2022-04-26 21:36:39 -0400

    fixed collisions settings

--- pokemon/game.js
+++ pokemon/game.js
@@ -16,25 +16,23 @@ let lastKey = '' //empty str for memory key
 
 
 const collisionsMap = []
-
-
 for (let i = 0; i<collisions.length; i+= 70){ //70 bc its the map tile width!!
     
     collisionsMap.push(collisions.slice(i, i+70))
 }
-
+pixels = 40.5 //pixel size of tiles after zooming in at 400% == x(original pixels) * 4 (400%)
 class Boundary {
-    static width = 48
-    static height = 48
-    constructor({pos}) {
-        this.pos = pos
-        this.width = 48 //pixel size of tiles after zooming in at 400% == 12(original pixels) * 4 (40%)
-        this.height = 48
+    static width = pixels
+    static height = pixels 
+    constructor({position}) {
+        this.position = position
+        this.width = pixels 
+        this.height = pixels 
     }
 
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.pos.x, this.pos.y, this.width, this.y)
+        c.fillStyle = 'black'
+        c.fillRect(this.position.x, this.position.y, this.width,this.height)//this.width, this.height)
     }
 }
 
@@ -46,15 +44,20 @@ const offset = { //syncing background + collisions pos
 
 collisionsMap.forEach((row, i ) => {
     row.forEach((symbol, j) => { //j = index of row currently loopin over
-        if (symbol === 1029)
+        if (symbol > 0)  {
+
+        
+        console.log("yeehaw")
+
+            
             boundaries.push(
                 new Boundary({
-                    pos: {
-                        x: j * Boundary.width + offset.x, //found in static width
-                        y: i * Boundary.height + offset.y
+                    position: {
+                        x: j * Boundary.width + offset.x,// -210, //found in static width //210 is from bad previous programming? ur not supposed to need it but u fucked up somewhere so now ur left addin extra space manually
+                        y: i * Boundary.height + offset.y //-100
             }}))
         
-        
+        }
     })
 })
 console.log(boundaries)
@@ -75,6 +78,7 @@ const keys = {
 }
 
 
+
 class Sprite { //character movement
     constructor({pos, velocity, bckground}) {
         this.pos = pos //position
@@ -82,7 +86,7 @@ class Sprite { //character movement
     }
 
     draw(){
-        c.drawImage(this.bckground, this.pos.x, this.pos.y)// change starting position of background
+        c.drawImage(this.bckground,this.pos.x, this.pos.y)// change starting position of background
     }
 }
 
@@ -94,13 +98,21 @@ const background = new Sprite({pos: {
     bckground: bckground
 })
 
-
+const testBoundary  = new Boundary({
+    position: {
+        x:400,
+        y:400
+    }
+})
+const movables = [background,testBoundary]
 function animate() {
     window.requestAnimationFrame(animate) 
     background.draw()
-    boundaries.forEach(boundary => {//collisions drawn
-        boundary.draw()
-    }),
+    testBoundary.draw()
+
+    // boundaries.forEach(boundary => {//collisions drawn
+    //     boundary.draw()
+    // }),
    // playerImg.onload = () => {
         c.drawImage(
             playerImg,
@@ -116,21 +128,24 @@ function animate() {
             playerImg.height
         )
     //}
-    if (keys.ArrowUp.pressed && lastKey === 'ArrowUp'){ //bckground view goes down
+    if (keys.ArrowUp.pressed && lastKey === 'ArrowUp')
+    { //bckground view goes down
         background.pos.y += 3
-        console.log(" y: " , background.pos.y)
+        testBoundary.position.y +=3
     } 
     if (keys.ArrowDown.pressed && lastKey === 'ArrowDown'){//bckground view goes up
         background.pos.y -= 3
-        console.log(" y: " , background.pos.y)
+        testBoundary.position.y -=3
     } 
     if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft'){ //bckground view goes left
         background.pos.x += 3
-        console.log(" x: " , background.pos.x)
+        testBoundary.position.x +=3
+       // console.log(" x: " , background.pos.x)
     } 
     if (keys.ArrowRight.pressed && lastKey === 'ArrowRight'){ //bckground view goes left
         background.pos.x -= 3
-        console.log(" x: " , background.pos.x)
+        testBoundary.position.x -=3
+       // console.log(" x: " , background.pos.x)
     } 
 }
 
@@ -138,22 +153,22 @@ window.addEventListener('keydown', (e) => { // === mousePressed
     //console.log(e.key)
     switch (e.key) { //if keyPressed
         case 'ArrowUp':
-            console.log(" hihi up u go")
+           // console.log(" hihi up u go")
             keys.ArrowUp.pressed = true
             lastKey = 'ArrowUp'
             break
         case 'ArrowDown':
-            console.log(" huhu down u go")
+            //console.log(" huhu down u go")
             keys.ArrowDown.pressed = true
             lastKey = 'ArrowDown'
             break
         case 'ArrowLeft':
-            console.log(" hehe left u go")
+           // console.log(" hehe left u go")
             keys.ArrowLeft.pressed = true
             lastKey = 'ArrowLeft'
             break
         case 'ArrowRight':
-            console.log(" hoho right u go")
+          //  console.log(" hoho right u go")
             keys.ArrowRight.pressed = true
             lastKey = 'ArrowRight'
             break
@@ -164,19 +179,19 @@ window.addEventListener('keyup', (e) => { //equivalent to mouseReleased
     //console.log(e.key)
     switch (e.key) { //if keyPressed
         case 'ArrowUp':
-            console.log(" hihi up u go")
+            //console.log(" hihi up u go")
             keys.ArrowUp.pressed = false
             break
         case 'ArrowDown':
-            console.log(" huhu down u go")
+           // console.log(" huhu down u go")
             keys.ArrowDown.pressed = false
             break
         case 'ArrowLeft':
-            console.log(" hehe left u go")
+           // console.log(" hehe left u go")
             keys.ArrowLeft.pressed = false
             break
         case 'ArrowRight':
-            console.log(" hoho right u go")
+           // console.log(" hoho right u go")
             keys.ArrowRight.pressed = false
             break
     }
